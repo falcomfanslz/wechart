edfe7898181311f1dbe6e24814d170252b039c01
lizhuo <lizhuo@guazi.com>
2018-01-29 18:04:13 +0800

    weixin4j test

--- web/src/main/java/com/kiseki/wechart/controller/ApiController.java
+++ web/src/main/java/com/kiseki/wechart/controller/ApiController.java
@@ -7,14 +7,18 @@
  */
 package com.kiseki.wechart.controller;
 
-import com.kiseki.wechart.model.TextMessage;
 import com.kiseki.wechart.service.TextMessageService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.weixin4j.WeixinException;
+import org.weixin4j.spi.HandlerFactory;
+import org.weixin4j.spi.IMessageHandler;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * <br>
@@ -33,7 +37,10 @@ public class ApiController {
         return echostr;
     }
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public String postWx(HttpServletRequest httpServletRequest){
-        return textMessageService.getTextMessage(httpServletRequest);
+    public String postWx(HttpServletRequest httpServletRequest) throws IOException, WeixinException {
+        ServletInputStream in = httpServletRequest.getInputStream();
+        IMessageHandler messageHandler = HandlerFactory.getMessageHandler();
+        String result = messageHandler.invoke(in);
+        return result;
     }
 }
